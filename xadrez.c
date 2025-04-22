a6a7e4ab429fd1fe24624054de8cbafbd6e61583
Warley-boop <sinister5432@gmail.com>
2025-04-22 20:48:51 +0000

    usei void para recursividade

--- xadrez.c
+++ xadrez.c
@@ -6,15 +6,52 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    int n = 2; // movimento do cavalo
- 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++) {
-            printf("*");
-        }
-        printf("\n");
+void moverBispo(int casas) {
+    if (casas < 5) {
+        printf("direita, cima\n", casas);  
+        moverBispo(casas + 1);
     }
+}
+ 
+int main() {
+   moverBispo(5);
+
+
+
+   void moverTorre(int casa) {
+    if (casas < 5) {
+        printf("direita\n", casa);  
+        moverTorre(casa + 1);
+    }
+}
+ 
+int main() {
+   moverTorre(5);
+
+
+
+   void moverTorre(int casao) {
+    if (casas < 8) {
+        printf("esquerda\n", casao);  
+        moverTorre(casao + 1);
+    }
+}
+ 
+int main() {
+   moverTorre(8);
+
+
+
+   for (int i = 0; i <=2; i++) {
+    for (int j = 0; j <= 1; j++) {
+        printf("movimento do cavalo: %d\n", i * j);
+    }
+    printf("\n");
+}
 
     return 0;
+
+
+
+
 }
